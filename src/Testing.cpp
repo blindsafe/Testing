fc15d62274a0c0b9be79daa16bee99020ffcd70e
Donald E. Trummell <dtrummell@gmail.com>
2021-09-15 22:38:22 -0700

    Initial program build and check-in
    Hello word

---
+++ src/Testing.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Testing.cpp
+// Author      : Donald Trummell
+// Version     : 0.1.0
+// Copyright   : (c) 2021
+// Description : Test Harness for blindSafew
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Windy" << endl; // prints Windy
+	return 0;
+}
